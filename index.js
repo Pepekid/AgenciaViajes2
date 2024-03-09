32f82f168ce105356f3363893a3d0e9efaaca875
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2024-03-09 08:17:07 +0100

    bonectar base de datos mysql

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+	.then(() => console.log('Base de datos conectada'))
+	.catch(error => console.log(error));
 
 // Definir puerto
 const port = process.env.PORT || 4000;
@@ -11,11 +17,11 @@ app.set('view engine', 'pug');
 
 // Obtener el año actual
 app.use((req, res, next) => {
-    const year = new Date();
-    res.locals.actualYear = year.getFullYear();
-    res.locals.nombreSitio = "Agencia de Viajes";
+	const year = new Date();
+	res.locals.actualYear = year.getFullYear();
+	res.locals.nombreSitio = 'Agencia de Viajes';
 
-    next();
+	next();
 });
 
 // Definir la carpeta pública
@@ -26,5 +32,5 @@ app.use('/', router); // use soporta todos los verbos:POST,GET,PATH y DELETE
 
 // Arrancar el servidor
 app.listen(port, () => {
-    console.log(`Servidor funcionando en el puerto ${port}`);
+	console.log(`Servidor funcionando en el puerto ${port}`);
 });
